551d4c262d9b695ed5b67d1b75f5bbdb44a5afa5
Alejandro Moctezuma <moctezumaalejandro25@gmail.com>
2025-03-31 20:18:45 -0600

    Data normalization and euclidean distance

--- clasifier.py
+++ clasifier.py
@@ -6,30 +6,38 @@ import numpy as np
 import pandas as pd
 
 ## FUNCTIONS
-
-def algorithm():
-    pass
-
 def open_data(data):
-    with open(data, mode ='r')as file:
-        diabetes_data = csv.reader(file)
-        for lines in diabetes_data:
-            print(lines)
+    diabetes_data = pd.read_csv(data)
+    return diabetes_data
 
-def euclidean_distance():
-    pass
+def normalize_data(data):
+    #Extract numeric values
+    columns = data.iloc[:, :-1]
 
-def generate_output():
+    #Mean and Standard Deviation
+    mean_data = columns.mean()
+    std_data = columns.std()
+    
+    data_normalized = (columns - mean_data) / std_data
+    return data_normalized
+
+def euclidean_distance(value_1, value_2):
+    return np.sqrt(np.sum((value_1 - value_2) ** 2))
+
+def generate_csv(data):
     #TODO generate either a .txt or .pdf
     pass
 
-def main():
+def algorithm():
     print("Clasificador Knn")
     print("A01736353 Alejandro Daniel Moctezuma Cruz")
     
-    open_data('Data/Diabetes-Clasificacion.csv')
-    algorithm()
+    read_data = open_data('Data/Diabetes-Clasificacion.csv')
+    normalized_data = normalize_data(read_data)
+    #print(normalized_data)
+
+    k_value = int(input("Introduce el valor de k: "))
 
 
 #Main Execution
-main()
+algorithm()
